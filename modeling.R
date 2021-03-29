2abf5a99e36fff4f2755db9231343ec2a030fb01
Jason Smith <JasonRSmith99@gmail.com>
2021-03-28 22:25:02 -0400

    switched to measuring marginal effects of probit models. Added full interacted on GEDIV

--- modeling.R
+++ modeling.R
@@ -1,12 +1,12 @@
 library(tidyverse)
 library(texreg)
-library(lmtest)
-library(sandwich)
+library(mfx)
+
 
 load(here::here("data", "cps_clean.RData"))
 
 model_vars <- cps_supplement %>% 
-  select(PEINHOME, PRTAGE, PTDTRACE, PEHSPNON, GEDIV, PEEDUCA,
+  dplyr::select(PEINHOME, PRTAGE, PTDTRACE, PEHSPNON, GEDIV, PEEDUCA,
          PREXPLF, PRDISFLG, PWSSWGT)
 
 #releveling education variable
@@ -19,6 +19,11 @@ model_vars$PEEDUCA <- fct_other(model_vars$PEEDUCA, drop = c("Associate degree (
 model_vars$PEEDUCA <- fct_relevel(model_vars$PEEDUCA, levels = c("Less than High School", "High school or GED", "Some college (no degree)",
                                                                  "Associate's degree", "Bachelor's degree", "Master's degree",
                                                                  "Professional degree", "Doctorate degree"))
+
+#fully interacted model
+
+full_mod <- probitmfx(PEINHOME ~ GEDIV*PRTAGE + GEDIV*PTDTRACE + GEDIV*PEHSPNON + GEDIV*PEEDUCA + GEDIV*PREXPLF + GEDIV*PRDISFLG, data = model_vars)
+
 
 #separate data by region
 ne <- model_vars %>% 
@@ -51,54 +56,28 @@ pac <- model_vars %>%
 
 #probit model
 
-mod_ne <- glm(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, family = binomial(link = "probit"),
-              data = ne)
+mod_ne <- probitmfx(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, robust = TRUE, data = ne)
 
-mod_mid_alt <- glm(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, family = binomial(link = "probit"),
-              data = mid_alt)
+mod_mid_alt <- probitmfx(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, robust = TRUE, data = mid_alt)
 
-mod_enc <- glm(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, family = binomial(link = "probit"),
-              data = enc)
+mod_enc <- probitmfx(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, robust = TRUE, data = enc)
 
-mod_wnc <- glm(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, family = binomial(link = "probit"),
-              data = wnc)
+mod_wnc <- probitmfx(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, robust = TRUE, data = wnc)
 
-mod_sa <- glm(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, family = binomial(link = "probit"),
-              data = sa)
+mod_sa <- probitmfx(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, robust = TRUE, data = sa)
 
-mod_esc <- glm(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, family = binomial(link = "probit"),
-              data = esc)
+mod_esc <- probitmfx(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, robust = TRUE, data = esc)
 
-mod_wsc <- glm(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, family = binomial(link = "probit"),
-              data = wsc)
+mod_wsc <- probitmfx(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, robust = TRUE, data = wsc)
 
-mod_mnt <- glm(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, family = binomial(link = "probit"),
-              data = mnt)
+mod_mnt <- probitmfx(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, robust = TRUE, data = mnt)
 
-mod_pac <- glm(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, family = binomial(link = "probit"),
-              data = pac)
+mod_pac <- probitmfx(PEINHOME ~ PRTAGE + PTDTRACE + PEHSPNON + PEEDUCA + PREXPLF + PRDISFLG, robust = TRUE, data = pac)
 
-mod_ne <- coeftest(mod_ne, vcov. = vcovHC, type = "HC1")
-
-mod_mid_alt <- coeftest(mod_mid_alt, vcov. = vcovHC, type = "HC1")
-
-mod_enc <- coeftest(mod_enc, vcov. = vcovHC, type = "HC1")
-
-mod_wnc <- coeftest(mod_wnc, vcov. = vcovHC, type = "HC1")
-
-mod_sa <- coeftest(mod_sa, vcov. = vcovHC, type = "HC1")
-
-mod_esc <- coeftest(mod_esc, vcov. = vcovHC, type = "HC1")
-
-mod_wsc <- coeftest(mod_wsc, vcov. = vcovHC, type = "HC1")
-
-mod_mnt <- coeftest(mod_mnt, vcov. = vcovHC, type = "HC1")
-
-mod_pac <- coeftest(mod_pac, vcov. = vcovHC, type = "HC1")
 
 screenreg(list(mod_ne, mod_mid_alt, mod_enc,
                mod_wnc, mod_sa, mod_esc, 
-               mod_wsc, mod_mnt, mod_pac), custom.coef.names = c("(Intercept)", "Age", "Black", "American Indian/Alaskan Native", "Asian", "Hawaiian/Pacific Islander",
+               mod_wsc, mod_mnt, mod_pac), custom.coef.names = c("Age", "Black", "American Indian/Alaskan Native", "Asian", "Hawaiian/Pacific Islander",
                                         "Race Other", "Non-Hispanic", "Highschool or GED", "Some college", "Associate's", "Bachelor's", "Master's", "Professional", "Docterate", "Unemployed", "No Disability"),
           custom.model.names = c("New England", "Mid Altlantic", "East North Central", 
                                  "West North Central", "South Atlantic", "East South Central", 
@@ -108,7 +87,7 @@ screenreg(list(mod_ne, mod_mid_alt, mod_enc,
 
 texreg(list(mod_ne, mod_mid_alt, mod_enc,
                mod_wnc, mod_sa, mod_esc, 
-               mod_wsc, mod_mnt, mod_pac), custom.coef.names = c("(Intercept)", "Age", "Black", "American Indian/Alaskan Native", "Asian", "Hawaiian/Pacific Islander",
+               mod_wsc, mod_mnt, mod_pac), custom.coef.names = c("Age", "Black", "American Indian/Alaskan Native", "Asian", "Hawaiian/Pacific Islander",
                                                                  "Race Other", "Non-Hispanic", "Highschool or GED", "Some college", "Associate's", "Bachelor's", "Master's", "Professional", "Docterate", "Unemployed", "No Disability"),
           custom.model.names = c("New England", "Mid Altlantic", "East North Central", 
                                  "West North Central", "South Atlantic", "East South Central", 
@@ -117,7 +96,7 @@ texreg(list(mod_ne, mod_mid_alt, mod_enc,
           digits = 4, stars = c(.01, .05, .1))
 
 
-car::Anova(c(mod_enc, mod_esc, mod_mid_alt, mod_mnt, mod_ne, mod_pac, mod_sa, mod_wnc, mod_wsc))
+anova(mod_enc, mod_esc, mod_mid_alt, mod_mnt, mod_ne, mod_pac, mod_sa, mod_wnc, mod_wsc)
 
 #TODO
 #post-hoc testing?
